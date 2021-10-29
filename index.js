af0e0509cb4a24f237ce16c08224215f923eda3e
Imran <imranimon1@gmail.com>
2021-10-30 00:19:34 +0200

    order create and get done

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ async function run() {
         console.log('Database Connected');
         const database = client.db("NFamTravel");
         const serviceCollection = database.collection("services");
+        const orderCollection = database.collection("orders")
         app.get('/db', (req, res) => {
             res.send('Database connected')
         })
 
+        //Service related api
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find({})
             const services = await cursor.toArray()
@@ -35,9 +37,33 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await serviceCollection.findOne(query);
-            console.log(result)
             res.json(result);
         })
+
+
+        //Order related api
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.json(result)
+        })
+
+        app.get('/orders', async (req, res) => {
+            const cursor = orderCollection.find({})
+            const orders = await cursor.toArray()
+            res.json(orders);
+        })
+
+        app.get('/orders/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const cursor = await orderCollection.find(query);
+            const orders = await cursor.toArray()
+            res.json(orders);
+        })
+
+
+
 
     } finally {
         // await client.close();
